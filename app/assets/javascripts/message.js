9091c322ce8287e7d0dcb888d65125270652a6d3
ayaa-machida <mamamamacchan@gmail.com>
2018-10-08 18:23:11 +0900

    非同期通信の実装
    # WHAT
    メッセージ送信機能に非同期通信を実装した。
    
    # WHY
    Ajaxを使って操作性の高いページを作るため。
    （本日コメントいただいた部分を修正しました。）

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,43 +1,26 @@
 $(function(){
   function buildHTML(message){
-    if(message.image.url !== null ){
-      var html = `<p>
-                    <div class= "messagelist">
-                      <div class= "messagelist__create">
-                        <div class= "messagelist__name">
-                          ${message.user_name}
+    var html_common = `<div class= "messagelist">
+                        <div class= "messagelist__create">
+                          <div class= "messagelist__name">
+                            ${message.user_name}
+                          </div>
+                          <div class= "messagelist__post_date">
+                            ${message.created_at}
+                          </div>
                         </div>
-                        <div class= "messagelist__post_date">
-                          ${message.created_at}
-                        </div>
-                      </div>
-                      <div class= "messagelist__comment">
-                        <p class="lower-message__content">
-                          ${message.content}
-                        </p>
-                          <image class= "lower-message__image", src= ${message.image.url}>
-                      </div>
-                    </div>
-                  </p>`
+                        <div class= "messagelist__comment">
+                          <p class="lower-message__content">
+                            ${message.content}
+                          </p>`
+    if(message.image.url === null ){
+      var html = html_common + `</div>
+                </div>`
       return html;
     }else{
-      var html = `<p>
-                    <div class= "messagelist">
-                      <div class= "messagelist__create">
-                        <div class= "messagelist__name">
-                          ${message.user_name}
-                        </div>
-                        <div class= "messagelist__post_date">
-                          ${message.created_at}
-                        </div>
-                      </div>
-                      <div class= "messagelist__comment">
-                        <p class="lower-message__content">
-                          ${message.content}
-                        </p>
-                      </div>
-                    </div>
-                  </p>`
+      var html = html_common + `<img class= "lower-message__image", src= ${message.image.url}>
+                  </div>
+                </div>`
       return html;
     }
   }
@@ -51,12 +34,13 @@ $(function(){
       data: formData,
       dataType: 'json',
       processData: false,
-      contentType: false
+      contentType: false,
     })
     .done(function(data){
       var html = buildHTML(data);
-      $('.messageslists').append(html)
-      $('.form__message').val('')
+      $('.messageslists').append(html);
+      $('.form__message').val('');
+      $('.hidden').val('');
       $('.form__submit').prop('disabled', false);
       $('.messageslists').animate({scrollTop:$('.messageslists')[0].scrollHeight}, 'fast');
     })
@@ -66,4 +50,3 @@ $(function(){
     })
   })
 });
-
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,7 +9,6 @@ class MessagesController < ApplicationController
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-      # binding.pry
       respond_to do |format|
         format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました' }
         format.json
