e15f58f74d0f95d00d128bf9f2a43611f2fd2d4d
ayaa-machida <mamamamacchan@gmail.com>
2018-10-10 20:06:32 +0900

    インクリメンタルサーチの実装
     # What
    チャットグループの新規登録や編集をする際に、チャットメンバーの検索をインクリメンタルサーチし、候補を表示する。
    
     # Why
    検索を非同期で行えるようにすることで、検索をするたびに検索結果画面にリダイレクトしないようにし、ビューが毎回再描画されるのを防ぐ。
    （コメントいただいた箇所を修正しました。）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id: current_user.id)
     respond_to do |format|
       format.html
       format.json
