11ab5d9e857152f396f8afed1777e3b262de7734
ayaa-machida <mamamamacchan@gmail.com>
2018-10-17 18:34:34 +0900

    S3に関する追記

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,13 +1,13 @@
+# encoding: utf-8
+
 class ImageUploader < CarrierWave::Uploader::Base
+
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
-
-  process resize_to_fit: [800, 800]
+  # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
